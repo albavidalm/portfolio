fe988447f1398a188ea80d4dd770335417aef484
albavidalm <albavidalm@gmail.com>
2022-07-14 17:31:45 +0200

    Add about and title2 styles

--- src/components/About.js
+++ src/components/About.js
@@ -6,43 +6,48 @@ import resume from "../assets/files/CV_Vidal_Moscardo_ENG.pdf";
 const About = () => {
   return (
     <section className="about wrapper" id="about">
-      <div className="container__title">
-        <h2 className="title title__2">About me</h2>
-        <div className="line"></div>
-      </div>
-      <div className="about__container--image">
-        <img className="about__image" alt="This is Alba" src={albapicture} />
-      </div>
-      <p className="about__text">
-        Hi! My name is Alba and I like to create online things. Although I
-        didn't know what web development was yet, my interest in it started some
-        time ago when I was trying to customize Blogger and Tumblr themes.
-      </p>
-      <p className="about__text">
-        I focused my professional career on administration and business
-        management and during these years I have improved and enhanced my
-        ability to solve problems, organization, self-learning and adapting to
-        change (as when I went to work in a clothing shop in another country).
-      </p>
-      <p className="about__text">
-        Some time ago, after having made some WordPress websites, my programming
-        bug started growing again so I took some courses on my own (a vice I
-        still have) until I finally finished my Web Application Development
-        bootcamp at Adalab.
-      </p>
-      <p className="about__text">
-        If I'm not programming I'm probably reading, learning to play the piano,
-        doing Crossfit or looking for a weekend plan.
-      </p>
-      <div className="about__buttons">
-        <a href={resume} target="_blank" rel="noreferrer">
-          <Button className="resume">
-            <span></span>
-          </Button>
-        </a>
-        <a href="#contact">
-          <Button className="talk"></Button>
-        </a>
+      <h2 className="title title__2">About me</h2>
+
+      <div className="about__container--info">
+        <div className="about__container--image">
+          <img className="about__image" alt="This is Alba" src={albapicture} />
+        </div>
+
+        <div className="text">
+          <p className="about__text">
+            Hi! My name is Alba and I like to create online things. Although I
+            didn't know what web development was yet, my interest in it started
+            some time ago when I was trying to customize Blogger and Tumblr
+            themes.
+          </p>
+          <p className="about__text">
+            I focused my professional career on administration and business
+            management and during these years I have improved and enhanced my
+            ability to solve problems, organization, self-learning and adapting
+            to change (as when I went to work in a clothing shop in another
+            country).
+          </p>
+          <p className="about__text">
+            Some time ago, after having made some WordPress websites, my
+            programming bug started growing again so I took some courses on my
+            own (a vice I still have) until I finally finished my Web
+            Application Development bootcamp at Adalab.
+          </p>
+          <p className="about__text">
+            If I'm not programming I'm probably reading, learning to play the
+            piano, doing Crossfit or looking for a weekend plan.
+          </p>
+          <div className="about__buttons">
+            <a href={resume} target="_blank" rel="noreferrer">
+              <Button className="resume">
+                <span></span>
+              </Button>
+            </a>
+            <a href="#contact">
+              <Button className="talk"></Button>
+            </a>
+          </div>
+        </div>
       </div>
       <Skills />
     </section>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,7 @@
 const Projects = () => {
   return (
     <section className="projects wrapper" id="projects">
-      <h2 className="title title__2 pjts">Projects</h2>
-      <p>Projects....</p>
+      <h2 className="title title__2">Projects</h2>
     </section>
   );
 };
