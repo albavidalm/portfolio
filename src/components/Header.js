89e6060be33ddc71f1b8e6306ef2f4019566ba22
albavidalm <albavidalm@gmail.com>
2022-07-19 13:16:56 +0200

    Add header animations

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
 import NavBar from "./NavBar";
+import circle from "../assets/images/circle.svg";
 
 const Header = () => {
   return (
     <header className="header">
       <NavBar />
-      <h1 className="header__title">Alba</h1>
-      <h1 className="header__title">Vidal</h1>
-      <h2 className="header__profession">
-        Front end developer<span className="header__profession--span">.</span>
-      </h2>
+
+      <div className="hero">
+        <h1 className="hero__title--name title">Alba</h1>
+        <h1 className="hero__title--lastName title">Vidal</h1>
+
+        <div className="hero__profession--line border__up"></div>
+        <h2 className="hero__profession">
+          Front end developer
+          <img alt="hearbeat point" src={circle} className="hero__circle"></img>
+        </h2>
+        <div className="hero__profession--line border__down"></div>
+      </div>
     </header>
   );
 };
